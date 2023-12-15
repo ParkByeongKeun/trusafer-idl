ff41f502ce44f75cd4a11ba20271772bf87d087b
ParkByeongKeun <yot132@naver.com>
2023-12-15 11:58:03 +0900

    Delete RegistererListReq data group_uuid

--- maincontrol/main_control.pb.gw.go
+++ maincontrol/main_control.pb.gw.go
@@ -101,20 +101,9 @@ func local_request_MainControl_ReadRegisterer_0(ctx context.Context, marshaler r
 
 }
 
-var (
-	filter_MainControl_ReadRegistererList_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_MainControl_ReadRegistererList_0(ctx context.Context, marshaler runtime.Marshaler, client MainControlClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ReadRegistererListRequest
 	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_MainControl_ReadRegistererList_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
 
 	msg, err := client.ReadRegistererList(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -124,13 +113,6 @@ func request_MainControl_ReadRegistererList_0(ctx context.Context, marshaler run
 func local_request_MainControl_ReadRegistererList_0(ctx context.Context, marshaler runtime.Marshaler, server MainControlServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ReadRegistererListRequest
 	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_MainControl_ReadRegistererList_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
 
 	msg, err := server.ReadRegistererList(ctx, &protoReq)
 	return msg, metadata, err
