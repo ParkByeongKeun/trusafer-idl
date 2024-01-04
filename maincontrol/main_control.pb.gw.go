324a85499b45f1437a24e16cc4605878b1d0f90a
ParkByeongKeun <yot132@naver.com>
2024-01-04 11:27:46 +0900

    Delete ReadGroupList Data

--- maincontrol/main_control.pb.gw.go
+++ maincontrol/main_control.pb.gw.go
@@ -917,20 +917,9 @@ func local_request_MainControl_CreateGroup_0(ctx context.Context, marshaler runt
 
 }
 
-var (
-	filter_MainControl_ReadGroup_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_MainControl_ReadGroup_0(ctx context.Context, marshaler runtime.Marshaler, client MainControlClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ReadGroupRequest
 	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_MainControl_ReadGroup_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
 
 	msg, err := client.ReadGroup(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -940,13 +929,6 @@ func request_MainControl_ReadGroup_0(ctx context.Context, marshaler runtime.Mars
 func local_request_MainControl_ReadGroup_0(ctx context.Context, marshaler runtime.Marshaler, server MainControlServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ReadGroupRequest
 	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_MainControl_ReadGroup_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
 
 	msg, err := server.ReadGroup(ctx, &protoReq)
 	return msg, metadata, err
